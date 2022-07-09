62dbca4250e4037bc6050a9b765e56cf5f4fdc14
paulhidalgo26 <paulsb26@gmail.com>
2022-07-09 01:59:01 -0500

    decimales

--- src/app/models/sale.ts
+++ src/app/models/sale.ts
@@ -1,9 +1,10 @@
+import { DecimalPipe } from '@angular/common';
 import { SaleDetail } from './saleDetail';
 
 export interface Sale{
     id?: number;
     iduserClient: number;
     date?: Date;
-    total?: number;
+    total?: DecimalPipe;
     saleDetails: SaleDetail[];
 }
